7c70a66fe2c77eb38cd1ea37545c1be7c93ec34d
Kacper <kb439948@students.mimuw.edu.pl>
2023-08-27 21:35:25 +0200

    XD

--- joint_venture.cpp
+++ joint_venture.cpp
@@ -17,27 +17,45 @@ int main() {
     for(long i = 0; i < n; i++) {
         long a;
         cin >> a;
-        vec.push_back(a);
+        if(a > 0 &&  a < 100000001)
+            vec.push_back(a);
     }
-    sort(vec.begin(), vec.end());
-    long first = 0, second = n-1;
-    bool wasSolutionFound = false;
-    while(first < second) {
-        if(vec[first] + vec[second] == x) {
-            cout<<"yes "<<vec[first]<<" "<<vec[second];
-            wasSolutionFound = true;
-            break;
-        }
-        else if(vec[first] + vec[second] < x) {
-            first++;
-        }
-        else {
-            second--;
+    // sort(vec.begin(), vec.end());
+    // long first = 0, second = n-1;
+    // bool wasSolutionFound = false;
+    // while(first < second) {
+    //     if(vec[first] + vec[second] == x) {
+    //         cout<<"yes "<<vec[first]<<" "<<vec[second];
+    //         wasSolutionFound = true;
+    //         break;
+    //     }
+    //     else if(vec[first] + vec[second] < x) {
+    //         first++;
+    //     }
+    //     else {
+    //         second--;
+    //     }
+    // }
+    // if(!wasSolutionFound) {
+    //     cout<<"danger";
+    // }
+
+    long a, b, dif = -1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i != j) {
+                if(vec[i] + vec[j] == x && (abs(vec[i] - vec[j]) > dif)) {
+                    a = min(vec[i], vec[j]);
+                    b = max(vec[i], vec[j]);
+                    dif = b-a;
+                }
+            }
         }
     }
-    if(!wasSolutionFound) {
+    if(dif != -1) 
+        cout<<"yes "<<a<<" "<<b;
+    else
         cout<<"danger";
-    }
 
     return 0;
 }
