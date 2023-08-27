51ed8bec29cab23ba1d08607a3afb87d150074ad
Kacper Bal <kb439948@students.mimuw.edu.pl>
2023-08-27 18:50:35 +0200

    XD

---
+++ joint_venture.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<long> vec;
+
+    long x, n;
+    
+
+    cin >> x;
+    cin >> n;
+
+    x = x * 10000000;
+    for(long i = 0; i < n; i++) {
+        long a;
+        cin >> a;
+        vec.push_back(a);
+    }
+    sort(vec.begin(), vec.end());
+    long first = 0, second = n-1;
+    bool wasSolutionFound = false;
+    while(first < second) {
+        if(vec[first] + vec[second] == x) {
+            cout<<"yes "<<vec[first]<<" "<<vec[second];
+            wasSolutionFound = true;
+            break;
+        }
+        else if(vec[first] + vec[second] < x) {
+            first++;
+        }
+        else {
+            second--;
+        }
+    }
+    if(!wasSolutionFound) {
+        cout<<"danger";
+    }
+
+    return 0;
+}
